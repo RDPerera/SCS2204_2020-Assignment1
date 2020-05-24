083267bcfa154a593c09d7b1ffc32d44ea470d64
Dilan Perera <39415471+RDPerera@users.noreply.github.com>
2020-05-24 18:50:28 +0530

    Update Q2.scala

--- Q2.scala
+++ Q2.scala
@@ -1,4 +1,4 @@
 object Question2 extends App {
-  def volumeSphere(r : Double):Double=4/3*math.Pi*(scala.math.pow(r,3))
+  def volumeSphere(r : Double):Double=4.0/3.0*math.Pi*(scala.math.pow(r,3))
   print("Volume of a sphere with radius 5 : "+volumeSphere(5));
 }
